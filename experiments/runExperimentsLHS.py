1afb9fcfcd0fffb2e6f3f68868bc0c074aec88c6
angelara <32181439+angelara@users.noreply.github.com>
2020-02-20 21:52:42 +1100

    EET and SOBOL files separated

--- experiments/runExperimentsLHS.py
+++ experiments/runExperimentsLHS.py
@@ -2,7 +2,11 @@ import os, sys
 
 sys.path.append(r'{}\EMAworkbench'.format(os.getcwd()))
 
+<<<<<<<< HEAD:experiments/runExperimentsLHS.py
 from gr4spModelLHS import getModel
+========
+from gr4spModelSOBOL import getModel
+>>>>>>>> a792cb8 (EET and SOBOL files separated):experiments/runExperimentSOBOLs.py
 from datetime import date
 
 if __name__ == '__main__':
@@ -20,14 +24,24 @@ if __name__ == '__main__':
 
     ema_logging.log_to_stderr(ema_logging.INFO)
 
+<<<<<<<< HEAD:experiments/runExperimentsLHS.py
     with MultiprocessingEvaluator(model,n_processes=1) as evaluator:
+========
+    with MultiprocessingEvaluator(model,n_processes=24) as evaluator:
+>>>>>>>> a792cb8 (EET and SOBOL files separated):experiments/runExperimentSOBOLs.py
         ## Variance-based SA
         #results = evaluator.perform_experiments(scenarios=0, policies=1050, levers_sampling=SOBOL)
 
         ## Generate Variance-based SA Policies, and save them into a file
+<<<<<<<< HEAD:experiments/runExperimentsLHS.py
         #results = evaluator.perform_experiments(scenarios=0, policies=2200, levers_sampling=LHS, generate_policy_file_only=True)
 
         results = evaluator.perform_experiments(scenarios=0, policies=1, levers_sampling='policies.lhs.object')
+========
+        #results = evaluator.perform_experiments(scenarios=0, policies=1050, levers_sampling=SOBOL, generate_policy_file_only=True)
+
+        results = evaluator.perform_experiments(scenarios=0, policies=1, levers_sampling='policies.sobol.object')
+>>>>>>>> a792cb8 (EET and SOBOL files separated):experiments/runExperimentSOBOLs.py
 
         #Testing
         #results = evaluator.perform_experiments(scenarios=0, policies=2)
