68e0c425af40e00ffcb04deb81e0babe71eacd6e
angelara <32181439+angelara@users.noreply.github.com>
2020-07-03 23:07:25 +1000

    EET changes on upper and lower bound uncertainties

--- experiments/gr4spModelSOBOL.py
+++ experiments/gr4spModelSOBOL.py
@@ -20,13 +20,13 @@ def getModel():
     model.constants = [Constant('onsiteGeneration', 'Central')]
     # model.constants += [Constant('rooftopPV', 'residential')]
     model.constants += [Constant('priceChangePercentageBattery', 0)]
-    model.constants += [Constant('priceChangePercentageOcgt', 0)]
+    #model.constants += [Constant('priceChangePercentageOcgt', 0)]
     model.constants += [Constant('priceChangePercentageCcgt', 0)]
     model.constants += [Constant('capacityFactorChangeBattery', 0)]
     model.constants += [Constant('capacityFactorChangeOcgt', 0)]
     model.constants += [Constant('capacityFactorChangeCcgt', 0)]
     model.constants += [Constant('capacityFactorChangeWind', 0)]
-    model.constants += [Constant('scheduleMinCapMarketGen', 30)]
+    #model.constants += [Constant('scheduleMinCapMarketGen', 30)]
 
 
 # set levers
@@ -47,44 +47,45 @@ def getModel():
 #         }
 #         return switcher.get(i,"invalid category")
 
-    # Uncertainties
+    # Uncertainties (4July2020 23 significant factors after EET using the union normalised mu* and sigma max and median values)
     model.uncertainties = [RealParameter('annualCpi', 0.01, 0.05)]
     model.uncertainties += [RealParameter('annualInflation', 0.01, 0.05)]
     model.uncertainties += [CategoricalParameter('consumption', ['Central', 'Slow change', 'Fast change', 'Step change', 'High DER'])]
-    model.uncertainties += [RealParameter('learningCurve', 0.0, 0.05)]
+    model.uncertainties += [RealParameter('learningCurve', 0.0, 0.1)]
     # variation of contribution of networks, retail and other charges in the tariff
     model.uncertainties += [IntegerParameter('wholesaleTariffContribution', 11, 45)]
 
-    model.uncertainties += [CategoricalParameter('energyEfficiency', ['Central', 'Slow change', 'Fast change', 'Step change', 'High DER'])]
+    model.uncertainties += [CategoricalParameter('energyEfficiency', ['Central', 'Slow change', 'Step change'])]
     model.uncertainties += [CategoricalParameter('solarUptake', ['Central', 'Slow change', 'Fast change', 'Step change', 'High DER'])]
     model.uncertainties += [CategoricalParameter('rooftopPV', ['residential', 'business', 'both'])]
 
-    model.uncertainties += [IntegerParameter('includePublicallyAnnouncedGen', 0, 2)]
-    model.uncertainties += [IntegerParameter('generationRolloutPeriod', 0, 11)]
-    model.uncertainties += [IntegerParameter('generatorRetirement', -5, 6)]
+    model.uncertainties += [IntegerParameter('includePublicallyAnnouncedGen', 0, 1)]
+    model.uncertainties += [IntegerParameter('generationRolloutPeriod', 0, 10)]
+    model.uncertainties += [IntegerParameter('generatorRetirement', -5, 5)]
     model.uncertainties += [RealParameter('technologicalImprovement', 0.0, 0.1)]
 
     # The variation on LCOEs are achieved increasing or decreasing a percentage depending on the type of fuel
     # This could represent a subsidy
     # model.levers += [IntegerParameter('priceChangePercentageBattery', -30, 30)]
-    model.uncertainties += [IntegerParameter('priceChangePercentageBrownCoal', -30, 31)]
-    # model.levers += [IntegerParameter('priceChangePercentageOcgt', -30, 30)]
+    model.uncertainties += [IntegerParameter('priceChangePercentageBrownCoal', -30, 30)]
+    model.uncertainties += [IntegerParameter('priceChangePercentageOcgt', -30, 30)]
     # model.levers += [IntegerParameter('priceChangePercentageCcgt', -30, 30)]
-    model.uncertainties += [IntegerParameter('priceChangePercentageWind', -30, 31)]
-    model.uncertainties += [IntegerParameter('priceChangePercentageWater', -30, 31)]
+    model.uncertainties += [IntegerParameter('priceChangePercentageWind', -30, 30)]
+    model.uncertainties += [IntegerParameter('priceChangePercentageWater', -30, 30)]
 
     # variation of min and max capacity factors as a percentage of current values
     # model.levers += [IntegerParameter('capacityFactorChangeBattery', -10, 10)]
-    model.uncertainties += [IntegerParameter('capacityFactorChangeBrownCoal', -10, 11)]
+    model.uncertainties += [IntegerParameter('capacityFactorChangeBrownCoal', -10, 10)]
     # model.levers += [IntegerParameter('capacityFactorChangeOcgt', -10, 10)]
     # model.levers += [IntegerParameter('capacityFactorChangeCcgt', -10, 10)]
     # model.levers += [IntegerParameter('capacityFactorChangeWind', -10, 10)]
-    model.uncertainties += [IntegerParameter('capacityFactorChangeWater', -10, 11)]
+    model.uncertainties += [IntegerParameter('capacityFactorChangeWater', -10, 10)]
 
 
 
 # arenas
-    # model.levers += [IntegerParameter('scheduleMinCapMarketGen', 10, 30)]
+
+    model.uncertainties += [IntegerParameter('scheduleMinCapMarketGen', 10, 30)]
     model.uncertainties += [CategoricalParameter('semiScheduleGenSpotMarket', ['primary', 'secondary', 'none'])]
     model.uncertainties += [RealParameter('semiScheduleMinCapMarketGen', 0.1, 30)]
     model.uncertainties += [CategoricalParameter('nonScheduleGenSpotMarket', ['primary', 'secondary', 'none'])]
