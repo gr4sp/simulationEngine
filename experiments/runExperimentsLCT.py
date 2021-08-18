861d0e5ef50e021280be2aec15e700b275921f47
angelara <angel.rouges@gmail.com>
2021-08-18 12:47:44 +0000

    keep version in the cloud

--- experiments/runExperimentsLCT.py
+++ experiments/runExperimentsLCT.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     with MultiprocessingEvaluator(model,n_processes=79) as evaluator:
 
-        results = evaluator.perform_experiments(scenarios=6, policies=0, uncertainty_sampling='uncertainties.morris.object',)#, uncertainty_sampling=FF , generate_experiments_file_only=True)
+        results = evaluator.perform_experiments(scenarios=6, policies=0, uncertainty_sampling='uncertainties.LCTff.object') 
 
     '''
     Print Results
